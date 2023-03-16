e39cd98a7b5ec2714cb44529642f00b1b6b6a269
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-16 21:54:22 +0600

    phone icon changed

--- src/pages/Home/InfoSection.js
+++ src/pages/Home/InfoSection.js
@@ -23,7 +23,7 @@ const InfoSection = () => {
       </div>
       <div className='h-[190px] bg-gradient-to-r from-[#19D3AE] to-[#0FCFEC] rounded-lg text-white flex gap-6 justify-center items-center px-4'>
         <div>
-          <i className="fa-solid fa-phone text-5xl"></i>
+          <i className="fa-solid fa-phone-volume text-5xl"></i>
         </div>
         <div>
           <h4 className='text-2xl'>Openning Hours</h4>
