1353c99d5269d43fcd03512f4a5b964013d16156
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-16 22:06:22 +0600

    made tablet responsive

--- src/pages/Home/AppointmentSection.js
+++ src/pages/Home/AppointmentSection.js
@@ -11,12 +11,12 @@ const AppointmentSection = () => {
                 <div className='text-white md:w-[70%] py-6 px-4 md:px-0 w-full'>
                     <h5 className='text-[#19D3AE] text-lg mb-4'>Appointments</h5>
                     <h1 className='text-3xl mb-2'>Make an appointment Today</h1>
-                    <p className='mb-2'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <p className='mb-2 opacity-100'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <Link to='appointment' className='text-start'>
                         <PrimaryButton >Get started</PrimaryButton>
                     </Link>
                 </div>
-                <div className='hidden md:block text-left'>
+                <div className='hidden lg:block text-left'>
                     <img src={doctor} alt="" className='mt-[-220px]' />
                 </div>
             </div>
